c911192dcf4517666c05eba18c22a025685a6342
Leofesk <leofesk@gmail.com>
2017-02-22 15:59:13 +0300

    Added new menu block for application: Settings.

---
+++ src/com/leofesk/quicktodomanager/view/settings/GeneralFrame.java
@@ -0,0 +1,100 @@
+package com.leofesk.quicktodomanager.view.settings;
+
+import com.leofesk.quicktodomanager.view.MainFrame;
+
+import javax.swing.*;
+
+public class GeneralFrame extends JFrame {
+
+    private JButton jButton1;
+    private JButton jButton2;
+    private JComboBox<String> jComboBox1;
+    private JLabel jLabel1;
+    private JPanel jPanel1;
+    private static ImageIcon imageAppIcon;
+    private String pathToAppLogo = "/img/AppLogo.png";
+
+    public GeneralFrame() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        jPanel1 = new JPanel();
+        jLabel1 = new JLabel();
+        jComboBox1 = new JComboBox<>();
+        jButton1 = new JButton();
+        jButton2 = new JButton();
+
+        imageAppIcon = new ImageIcon(MainFrame.class.getResource(pathToAppLogo));
+        setIconImage(imageAppIcon.getImage());
+
+        setTitle(" General Options");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setResizable(false);
+
+        jPanel1.setBorder(BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jLabel1.setText("Language:");
+
+        jComboBox1.setModel(new DefaultComboBoxModel<>(new String[]{"English", "Русский"}));
+
+        GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(jLabel1)
+                                .addGap(10, 10, 10)
+                                .addComponent(jComboBox1, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE)
+                                .addGap(10, 10, 10))
+        );
+
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(jComboBox1)
+                                        .addComponent(jLabel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(10, 10, 10))
+        );
+
+        jButton1.setText("Save");
+
+        jButton2.setText("Cancel");
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(10, 10, 10))
+                                        .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                .addComponent(jButton2)
+                                                .addGap(10, 10, 10)
+                                                .addComponent(jButton1)
+                                                .addGap(10, 10, 10))))
+        );
+
+        layout.setVerticalGroup(
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addGap(10, 10, 10)
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jButton1)
+                                        .addComponent(jButton2))
+                                .addGap(10, 10, 10))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+}
